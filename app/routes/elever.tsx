15f094f7092f97538c0db68ee2409e8d93f99b7b
Hknots <hknots99@gmail.com>
2024-06-24 14:52:52 +0200

    add dummy data & setup basic structure

--- app/routes/elever.tsx
+++ app/routes/elever.tsx
@@ -1,39 +1,76 @@
-import {Table} from "@navikt/ds-react";
+import {Button, Table} from "@navikt/ds-react";
+import {PersonPlusIcon} from "@navikt/aksel-icons";
 
 const data = [
     {
         name: "Jakobsen, Markus",
-        percent: 100,
+        birthNumber: 9543049930,
+        email: "test@gmail.com",
+        mobileNumber: 45399999,
+        averageGrades: 4.23
     },
     {
-        name: "Halvorsen, Mari",
-        percent: 50,
+        name: "Jakobsen, Markus",
+        birthNumber: 9543049930,
+        email: "test@gmail.com",
+        mobileNumber: 45399999,
+        averageGrades: 4.23
     },
     {
-        name: "Christiansen, Mathias",
-        percent: 95,
+        name: "Jakobsen, Markus",
+        birthNumber: 9543049930,
+        email: "test@gmail.com",
+        mobileNumber: 45399999,
+        averageGrades: 4.23
     },
     {
-        name: "Fredriksen, Leah",
-        percent: 100,
+        name: "Jakobsen, Markus",
+        birthNumber: 9543049930,
+        email: "test@gmail.com",
+        mobileNumber: 45399999,
+        averageGrades: 4.23
     },
-    {
-        name: "Evensen, Jonas",
-        percent: 9,
-    },
+];
+
+const columns = [
+    { name: "Navn", width: "20px" },
+    { name: "Fødselsnummer", width: "1000px" },
+    { name: "Email", width: "40px" },
+    { name: "Telefonnummer", width: "200px" },
+    { name: "Karakter Gjenomsnitt", width: "20px" },
 ];
 
 export default function Elever() {
     return (
-        <Table>
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell scope="col">Navn</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Fødselsnummer</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Telefonnummer</Table.HeaderCell>
-                    <Table.HeaderCell scope="col">Karakter Gjenomsnitt</Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
-        </Table>
-    )
+        <div className="p-5">
+            <h2 className="font-bold text-3xl">Elever</h2>
+            <Button icon={<PersonPlusIcon/>}>Legg til elev</Button>
+            <Table>
+                <Table.Header>
+                    <Table.Row>
+                        {columns.map((column) => {
+                            return (
+                                <Table.HeaderCell key={column.name} scope="col">
+                                    {column.name}
+                                </Table.HeaderCell>
+                            )
+                        })}
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {data.map((row, i) => {
+                        return (
+                            <Table.Row key={i}>
+                                <Table.HeaderCell scope="row">{row.name}</Table.HeaderCell>
+                                <Table.DataCell>{row.birthNumber}</Table.DataCell>
+                                <Table.DataCell>{row.email}</Table.DataCell>
+                                <Table.DataCell>{row.mobileNumber}</Table.DataCell>
+                                <Table.DataCell>{row.averageGrades}</Table.DataCell>
+                            </Table.Row>
+                        );
+                    })}
+                </Table.Body>
+            </Table>
+        </div>
+    );
 }
