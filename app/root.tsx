36e3c70dfa0ce1e538555149b6874faf41a60f22
Hknots <hknots99@gmail.com>
2024-06-24 12:37:59 +0200

    move layout to actualy layout of root

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,60 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import {json, Links, Meta, Outlet, Scripts, ScrollRestoration,} from "@remix-run/react";
 import "./tailwind.css";
+import {LoaderFunction, MetaFunction} from "@remix-run/node";
+import {Button} from "@navikt/ds-react";
+import {LeaveIcon} from "@navikt/aksel-icons";
+import {useLoaderData} from "react-router";
 
-export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+export const meta: MetaFunction = () => {
+    return [
+        {title: "New Remix App"},
+        {name: "description", content: "Welcome to Remix!"},
+    ];
+};
+
+export const loader: LoaderFunction = async ({request}) => {
+    const username = "Henrik Knutsen";
+    return json({username});
+};
+
+export function Layout({children}: { children: React.ReactNode }) {
+    const {username} = useLoaderData();
+
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <Meta/>
+            <Links/>
+        </head>
+        <body>
+        <div className="w-screen h-screen bg-gray-200 flex flex-col">
+            <header className="w-full bg-gray-400 flex h-20 justify-center">
+                <div className="w-3/4 mx-auto bg-gray-500 flex flex-row items-center p-4 justify-between">
+                    <div className="flex flex-row gap-6">
+                        <div>Logo here</div>
+                        <Button>Hjem</Button>
+                        <Button>Elever</Button>
+                        <Button>Logg</Button>
+                    </div>
+                    <div className="flex flex-row gap-2 items-center">
+                        <div>{username}</div>
+                        <Button><LeaveIcon/></Button>
+                    </div>
+                </div>
+            </header>
+            <main className="mx-auto m-10 w-3/4 h-1/2 bg-gray-400 flex">
+                {children}
+            </main>
+        </div>
+        <ScrollRestoration/>
+        <Scripts/>
+        </body>
+        </html>
+    );
 }
 
 export default function App() {
-  return <Outlet />;
+    return <Outlet/>;
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,32 +1,5 @@
-import type {MetaFunction} from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-    return [
-        {title: "New Remix App"},
-        {name: "description", content: "Welcome to Remix!"},
-    ];
-};
-
 export default function Index() {
     return (
-        <div className="w-screen h-screen bg-gray-200 flex flex-col">
-            <header className="w-full bg-gray-400 flex h-20 justify-center">
-                <div className="w-3/4 mx-auto bg-gray-500 flex flex-row items-center p-4 justify-between">
-                    <div className="flex flex-row gap-6">
-                        <div>Logo here</div>
-                        <h1>Hjem</h1>
-                        <h1>Elever</h1>
-                        <h1>Logg</h1>
-                    </div>
-                    <div className="flex flex-row gap-2">
-                        <div>Brukernavn</div>
-                        <div>Logg ut icon</div>
-                    </div>
-                </div>
-            </header>
-            <main className="mx-auto m-10 w-3/4 h-1/2 bg-gray-400 flex">
-
-            </main>
-        </div>
+        <div>hi</div>
     );
 }
